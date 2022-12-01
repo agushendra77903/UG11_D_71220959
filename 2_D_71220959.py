6ace47a0297f77b698b6d51488e05f1b355d06ac
agushendra77903 <117900786+agushendra77903@users.noreply.github.com>
2022-12-01 16:13:45 +0700

    Add files via upload

---
+++ 2_D_71220959.py
@@ -0,0 +1,5 @@
+huruf = input ("masukkan kata :")
+if huruf % 2 == 0:
+    print("%i huruf paling ujung pada kata",huruf,"adalah" % huruf)
+else: 
+    print("%i huruf paling ujung pada kata",huruf,"adalah" % huruf)
